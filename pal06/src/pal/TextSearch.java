571b4b8b845b80649f8977415a4348dcc7fe67ac
Thang Do <pandothang@gmail.com>
2015-12-22 21:52:21 +0100

    passed 9/10

--- pal06/src/pal/TextSearch.java
+++ pal06/src/pal/TextSearch.java
@@ -36,7 +36,7 @@ public class TextSearch {
 		levensteinPath = new int[M + 1][N + 1];
 
 		for (int i = 0; i < M + 1; i++) {
-			levenstein[i][0] = i;
+			levenstein[i][0] = i*cD;
 		}
 
 		for (int i = 0; i < N + 1; i++) {
@@ -72,31 +72,29 @@ public class TextSearch {
 	}
 
 	void findSubstring() {
-		int min;
-		min = levenstein[M][M];
-		int indexOfMin = M;
-		for (int i = M; i < N + 1; i++) {
-			if (levenstein[M][i] < min) {
-//				System.out.println("found" + levenstein[M][i]);
-				min = levenstein[M][i];
-				indexOfMin = i;
-			}
+		int indexOfMin = -1;
+		int start = 0;
+		int minLeft = -1;
+		while (minLeft == -1) {
+			indexOfMin = findMinInLevenstein(start);
+			minLeft = levensteinReverse(indexOfMin);
+			start = indexOfMin;
 		}
+
 		ArrayList<Integer> minimums = new ArrayList<>();
 		minimums.add(indexOfMin);
-		for (int i = indexOfMin + 1; i < N + 1; i++) {
-			if (levenstein[M][i] == min) {
+		for (int i = indexOfMin + 1; i < N; i++) {
+			if (levenstein[M][i] == levenstein[M][indexOfMin]) {
 				minimums.add(i);
 //				System.out.println(i);
 			}
 		}
 		int minimumLength = -1;
-		int leftIndex = -1;
+		int leftIndex = -5;
 
 		for (int i = 0; i < minimums.size(); i++) {
-			int fooLeftIndex = levensteinReverse(minimums.get(i)) + 1;
-			int fooMinLength = minimums.get(i) - fooLeftIndex  + 1;
-
+			int fooLeftIndex = levensteinReverse(minimums.get(i));
+			int fooMinLength = minimums.get(i) - fooLeftIndex;
 //			System.out.println(fooLeftIndex + " left index");
 //			System.out.println(fooMinLength + " min Length");
 //			System.out.println(fooLeftIndex);
@@ -105,31 +103,46 @@ public class TextSearch {
 				minimumLength = fooMinLength;
 				leftIndex = fooLeftIndex;
 			} else {
-				if (fooLeftIndex < leftIndex) {
+				if (fooMinLength < minimumLength) {
 					minimumLength = fooMinLength;
 					leftIndex = fooLeftIndex;
 				}
 			}
 
 		}
-		System.out.println(leftIndex - 1 + " " + minimumLength + " " + min);
+		System.out.println(leftIndex + " " + minimumLength + " " + levenstein[M][indexOfMin]);
+	}
+
+	int findMinInLevenstein(int start) {
+		int min = levenstein[M][start + 1];
+		int indexOfMin = start + 1;
+		for (int i = start + 1; i < N + 1; i++) {
+			if (levenstein[M][i] < min) {
+//				System.out.println("found" + levenstein[M][i]);
+				min = levenstein[M][i];
+				indexOfMin = i;
+			}
+		}
+		return indexOfMin;
 	}
 
 	int levensteinReverse(int start) {
 //		System.out.println(start);
 		int i = M;
 		int k = start;
+//		System.out.println("k = " + start + " i = " + i);
 		while (i > 0) {
 			if (k == 0) {
 				break;
 			}
-//			System.out.println(i);
-//			System.out.println(k);
-			int movedCode = findIndexMin(
-				levenstein[i - 1][k] + cD,
-				levenstein[i][k - 1] + cI,
-				levenstein[i - 1][k - 1] + (pattern.charAt(i - 1) == text.charAt(k - 1) ? 0 : cR)
-			);
+
+//			int movedCode = findIndexMin(
+//				levenstein[i - 1][k] + cD,
+//				levenstein[i][k - 1] + cI,
+//				levenstein[i - 1][k - 1] + (pattern.charAt(i - 1) == text.charAt(k - 1) ? 0 : cR)
+//			);
+			int movedCode = levensteinPath[i][k];
+//			System.out.println("code is " + movedCode);
 			if (movedCode == 1) {
 				i--;
 			} else if (movedCode == 2) {
@@ -138,7 +151,13 @@ public class TextSearch {
 				i--;
 				k--;
 			}
+//			System.out.println(i + " " + k);
 		}
+//		System.out.println("__");
+		if (i != 0) {
+			return -1;
+		}
+//		System.out.println("leftmost " + k);
 		return k;
 	}
 
@@ -146,17 +165,17 @@ public class TextSearch {
 		int i = M;
 		int k = start;
 		return 1;
-		
+
 	}
 
 	private int findMin(int a, int b, int c) {
 		int min = a;
 
-		if (b < min) {
+		if (b <= min) {
 			min = b;
 		}
 
-		if (c < min) {
+		if (c <= min) {
 			min = c;
 		}
 
